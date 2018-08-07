f78f4caffffd767846ed8a143852a2d63e67b2f9
yuvipanda <yuvipanda@gmail.com>
2018-08-07 10:50:31 -0700

    Add grading for lab02 and lab03

--- grading/data8x.3-grade.bash
+++ grading/data8x.3-grade.bash
@@ -11,6 +11,18 @@ while 1; do
         lab01 \
         courses.edx.org-adcc1672ebcb43b7b62b72abaa434e9c
 
+    python3 rungrader.py \
+        '/mnt/prod-3-fileserver/{user_id}/materials-x18/materials/x18/lab/3/{lab}/{lab}.ipynb' \
+        '/srv/repo/materials/x18/lab/3/{lab}/{lab}.ipynb' \
+        lab02 \
+        courses.edx.org-d24302f703ff4024878da7f9dee6f734
+
+    python3 rungrader.py \
+        '/mnt/prod-3-fileserver/{user_id}/materials-x18/materials/x18/lab/3/{lab}/{lab}.ipynb' \
+        '/srv/repo/materials/x18/lab/3/{lab}/{lab}.ipynb' \
+        lab03 \
+        courses.edx.org-fe47f28fcffe42bc9c0f0bc83b1fa950
+
 
     sleep 1;
 done
